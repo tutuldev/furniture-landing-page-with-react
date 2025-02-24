633d23ab2b8634af06ae2ab828f34975a6af5a3a
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-24 10:51:08 +0600

    theme extends

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './App.css'
+import './Index.css'
 
 const App = () => {
   return (
     <>
-      <h1 className='text-5xl text-red-500 font-bold text-center p-20'>Hellow Developers</h1>
+      <h1 className='text-5xl font-primary text-red-500 font-bold text-center p-20'>Hellow Developers</h1>
+      <p className="text-primary text-5xl text-center">This is a custom text color!</p>
+
     </>
   );
 };
