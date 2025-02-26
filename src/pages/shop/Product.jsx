e9b05f62daeb40384c08400cc67c25b6c6e74878
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-26 17:43:32 +0600

    lodemore item

--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import { products } from '../../utils/products';
 import ProductCard from './ProductCard';
+import btnIcon from '../../assets/button-icon.png'
+
 
 const Product = ({headline}) => {
 
     const categories = ["Chair","Beds","Sofa","Lamp"]
     const [selectedCategory, SetSelectedCategory]= useState("Chair")
     const filteredProduct = products.filter((product)=>product.category === selectedCategory)
+    const [visiableProduct,setVisibleProducts] = useState(4)
+
+    const loadMoreProducts = ()=>{
+        setVisibleProducts((prev)=> prev + 4)
+    }
 
     return (
         <div>
@@ -22,6 +29,7 @@ const Product = ({headline}) => {
                                 <button 
                                 onClick={()=>{
                                     SetSelectedCategory(category);
+                                    setVisibleProducts(4)
                                 }}
                                  key={category} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-primary hover:text-white
                                     transition-colors ${selectedCategory === category ? "bg-white text-primary": "text-secondary"}`}>{category}</button>
@@ -33,11 +41,25 @@ const Product = ({headline}) => {
                 {/* product grid */}
                 <div className='grid grid-cols-1 sm:grid-cols2 md:grid-cols-4 gap-6'>
                     {
-                        filteredProduct.map((product,index) =>(
+                        filteredProduct.slice(0,visiableProduct).map((product,index) =>(
                             <ProductCard key={index} product={product}/>
                         ))
                     }
                 </div>
+
+                {/* loadmore button  */}
+                {
+                    visiableProduct <filteredProduct.length && (
+                        <div className='flex justify-center items-center mt-8'>
+                            <button
+                            onClick={loadMoreProducts}
+                            className='text-base font-semibold text-primary flex items-center gap-1'>
+                                                    View All
+                                    <img src={btnIcon} alt="" />
+                            </button>
+                        </div>
+                    )
+                }
 
             </div>
         </div>
