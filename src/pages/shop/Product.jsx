343ac37c8f8bf7a165c8f607e665f389b5d765b5
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-26 11:33:49 +0600

    show product by category

--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { products } from '../../utils/products';
 import ProductCard from './ProductCard';
 
 const Product = ({headline}) => {
 
     const categories = ["Chair","Beds","Sofa","Lamp"]
+    const [selectedCategory, SetSelectedCategory]= useState("Chair")
+    const filteredProduct = products.filter((product)=>product.category === selectedCategory)
 
     return (
         <div>
@@ -17,8 +19,12 @@ const Product = ({headline}) => {
                     <div className='flex flex-col sm:flex-row items-center md:justify-between justify-center gap-4'>
                         {
                             categories.map((category)=>(
-                                <button key={category} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-primary hover:text-white
-                                    transition-colors`}>{category}</button>
+                                <button 
+                                onClick={()=>{
+                                    SetSelectedCategory(category);
+                                }}
+                                 key={category} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-primary hover:text-white
+                                    transition-colors ${selectedCategory === category ? "bg-white text-primary": "text-secondary"}`}>{category}</button>
                             ))
                         }
                     </div>
@@ -27,7 +33,7 @@ const Product = ({headline}) => {
                 {/* product grid */}
                 <div className='grid grid-cols-1 sm:grid-cols2 md:grid-cols-4 gap-6'>
                     {
-                        products.map((product,index) =>(
+                        filteredProduct.map((product,index) =>(
                             <ProductCard key={index} product={product}/>
                         ))
                     }
